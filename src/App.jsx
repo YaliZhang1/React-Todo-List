b4e3ca493c1eb56a96940c5f4b3b727f45058e64
YaliZhang1 <yali.zhang@hyperisland.se>
2025-01-07 21:36:20 +0100

    add functions for items

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import "./App.css";
 
 export default class App extends Component {
+  //状态在哪里，操作状态的方法就在哪里
   // initialize the state of the component
   state = {
     todos: [
@@ -15,13 +16,37 @@ export default class App extends Component {
       // Add more todos here
     ],
   };
+  // addTodo for add one todo item, the parameter is todoObj
+  addTodo = (todoObj) => {
+    const { todos } = this.state; //get the old todos
+    const newTodos = [todoObj, ...todos]; //add one todo
+    //update status
+    this.setState({
+      todos: newTodos,
+    });
+  };
+
+  // updateTodo for update one todo Object
+  updateTodo = (id, done) => {
+    const { todos } = this.state; //get old todos
+    const newTodos = todos.map((todoObj) => {
+      if (todoObj.id === id)
+        //if id are the same, update the status of done to the new done status
+        return {
+          ...todoObj, //copy the old date
+          done, //only update the status of done to the new done status, here is done == done, but we can omit the second parameter done here.
+        };
+      else return todoObj;
+    });
+    this.setState({ todos: newTodos }); //update the status
+  };
   render() {
     const { todos } = this.state;
     return (
       <div className="todo-container">
         <div className="todo-wrap">
-          <Header />
-          <List todos={todos} />
+          <Header addTodo={this.addTodo} />
+          <List todos={todos} updateTodo={this.updateTodo} />
           <Footer />
           {/* Add your todo components here */}
           {/* <Todo /> */}
--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
 import "./index.css";
 export default class Item extends Component {
+  state = { mouse: false }; //the mouse in or out
+  // callback functions of the mouse in or out
+  handleMouse = (flag) => {
+    return () => {
+      this.setState({ mouse: flag });
+    };
+  };
+  // todo callback functions of the checked or cancelled checked
+  handleCheck = (id) => {
+    return (event) => {
+      this.props.updateTodo(id, event.target.checked);
+    };
+  };
   render() {
     const { id, name, done } = this.props;
+    const { mouse } = this.state;
     return (
-      <li>
+      <li
+        style={{ backgroundColor: mouse ? "#ddd" : "white" }}
+        onMouseEnter={this.handleMouse(true)}
+        onMouseLeave={this.handleMouse(false)}
+      >
         <label>
-          <input type="checkbox" defaultChecked={done} />
+          <input
+            type="checkbox"
+            defaultChecked={done}
+            onChange={this.handleCheck(id)}
+          />
           <span>{name}</span>
         </label>
-        <button className="btn btn-danger" style={{ display: "none" }}>
+        <button
+          className="btn btn-danger"
+          style={{ display: mouse ? "block" : "none" }}
+        >
           Delete
         </button>
       </li>
